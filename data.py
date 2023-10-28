bba3493fbbfa123b8a5cf4d4ea58fe30b5cfe3e1
Aidan T <87583248+15athompson@users.noreply.github.com>
2023-10-28 13:21:22 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+stock_data = pd.read_csv('./NFLX.csv', index_col='Date')
+stock_data.head()
+
+
+import matplotlib.dates as mdates
+import matplotlib.dates as plt
+
+import datetime as dt
+
+plt.figure(figsize=(15,10))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=60))
+x_dates = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in stock_data.index.values]
+
+plt.plot(x_dates, stock_data['High'], label='High')
+plt.plot(x_dates, stock_data['Low'], label='Low')
+plt.xlabel('Time Scale')
+plt.ylabel('Scaled USD')
+plt.legend()
+plt.gcf().autofmt_xdate()
+plt.show()
